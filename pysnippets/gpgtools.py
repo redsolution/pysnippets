de6fadf060b42388f7b9f918737ea43810085da7
Aleksey Osadchuk <aleksey.osadchuk@redsolution.ru>
2014-12-29 14:12:32 +0500

    set encoding to utf-8

--- pysnippets/gpgtools.py
+++ pysnippets/gpgtools.py
@@ -7,6 +7,7 @@ def check_data_sign(data):
     if not data:
         return None
     gpg = gnupg.GPG()
+    gpg.encoding = 'utf-8'
     if data.get('sign') and gpg.verify(data['sign']):
         return data['data']
     return None
@@ -14,6 +15,7 @@ def check_data_sign(data):
 
 def sign_data(data):
     gpg = gnupg.GPG()
+    gpg.encoding = 'utf-8'
     return gpg.sign(str(data))
 
 
@@ -23,6 +25,7 @@ def get_signed_json(data):
 
 def encrypt_data(data, key_name):
     gpg = gnupg.GPG()
+    gpg.encoding = 'utf-8'
     enc = gpg.encrypt(data, key_name)
     if enc.data and enc.status == 'encryption ok':
         return enc.data
@@ -34,4 +37,5 @@ def decrypt_data(data):
     if not data:
         return data
     gpg = gnupg.GPG()
+    gpg.encoding = 'utf-8'
     return gpg.decrypt(data).data
